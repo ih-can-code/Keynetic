5b65037dd04bb276237afe7f6918b2a012dca7bc
ih_can_code <hasan.byte14@gmail.com>
2026-09-13 17:41:02 +0600

    animation

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ const hero = document.querySelector("#hero");
 const typing = document.querySelector("#typing");
 const enterBtn = document.querySelector("#enter-btn");
 const typingInput = document.querySelector("#typing-input");
+const heroTitle = document.querySelector("#hero-title");
 let isTransitioning = false;
+
+function animateHeroTitle() {
+    const title = heroTitle.dataset.text;
+    let characterIndex = 0;
+
+    const typingAnimation = setInterval(() => {
+        heroTitle.textContent += title[characterIndex];
+        characterIndex += 1;
+
+        if (characterIndex === title.length) {
+            clearInterval(typingAnimation);
+            heroTitle.classList.add("is-typed");
+        }
+    }, 85);
+}
+
+animateHeroTitle();
 
 function startTyping() {
     if (isTransitioning) return;
